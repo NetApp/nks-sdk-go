9b7a51d48d70baaf766b0a6c9d21ee3f964d6022
Matt Baldwin <baldwin@stackpointcloud.com>
2016-08-29 18:20:27 -0700

    WIP
    Change-Id: Ibc36ed25e183818259b9baab6bf366df3a573a3f

---
+++ sdk.go
@@ -0,0 +1,7 @@
+// Package sdk is the official Stackpoint.io SDK for the Go programming language.
+//
+// See our Developer Guide for information for on getting started and using
+// the SDK.
+//
+// https://github.com/StackPointCloud/stackpoint-sdk-go
+package sdk
